ca470d21ea913f309351c7a648b7e756a098749b
mkvitko <marchello9307@gmail.com>
2019-02-04 22:01:19 +0200

    REM-984

--- src/main/java/io/remme/java/enums/Patterns.java
+++ src/main/java/io/remme/java/enums/Patterns.java
@@ -10,7 +10,9 @@ public enum Patterns {
     PUBLIC_KEY("^[a-f0-9]{66}$"),
     ADDRESS("^[a-f0-9]{70}$"),
     SWAP_ID("^[a-f0-9]{64}$"),
-    HEADER_SIGNATURE("^[a-f0-9]{128}$");
+    HEADER_SIGNATURE("^[a-f0-9]{128}$"),
+    SHA256("^[a-f0-9]{64}$"),
+    SHA512("^[a-f0-9]{128}$");
 
     private String pattern;
 }
